87c9cc8962038a761abe66e59b43ad27a8b8a597
Valentin-Crespo <valentinjosecrespo@gmail.com>
2023-11-06 00:08:43 -0300

    Añadi Nosotros, Fusione los html y arregle problemas

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 function validarFormulario() {
     let nombre = document.getElementById("nombre").value;
-    let mensaje = document.getElementById("mensaje").value;
+    let edad = document.getElementById("edad").value;
+    let dni = document.getElementById("dni").value;
+    let domicilio = document.getElementById("domicilio").value;
+    
+    document.getElementById("nombrei").innerHTML =nombre;
+    document.getElementById("edadi").innerHTML = edad;
+    document.getElementById("dnii").innerHTML = dni;
+    document.getElementById("domicilioi").innerHTML = domicilio;
 
-
-    if (nombre === "" || mensaje === "") {
+    if (nombre === "" || edad === "" || dni === "" || domicilio === "") {
       document.getElementById("resultado").innerHTML = "Por favor, complete todos los campos.";
     } else {
       document.getElementById("resultado").innerHTML = "Formulario enviado " 
       console.log(nombre);
-      console.log(mensaje);
+      console.log(edad);
+      console.log(dni)
+      console.log(domicilio)
   }
+    
 }
