c05a7e04fca90a98292f286427d24085303ab24b
Valentin-Crespo <valentinjosecrespo@gmail.com>
2023-11-06 22:44:21 -0300

    Cambios en el footer

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 function validarFormulario() {
     let nombre = document.getElementById("nombre").value;
-    let edad = document.getElementById("edad").value;
+    let entrega = document.getElementById("entrega").value;
     let dni = document.getElementById("dni").value;
     let domicilio = document.getElementById("domicilio").value;
     
     document.getElementById("nombrei").innerHTML =nombre;
-    document.getElementById("edadi").innerHTML = edad;
+    document.getElementById("entregai").innerHTML = entrega;
     document.getElementById("dnii").innerHTML = dni;
     document.getElementById("domicilioi").innerHTML = domicilio;
 
-    if (nombre === "" || edad === "" || dni === "" || domicilio === "") {
+    if (nombre === "" || entrega === "" || dni === "" || domicilio === "") {
       document.getElementById("resultado").innerHTML = "Por favor, complete todos los campos.";
     } else {
       document.getElementById("resultado").innerHTML = "Formulario enviado " 
       console.log(nombre);
-      console.log(edad);
+      console.log(entrega);
       console.log(dni)
       console.log(domicilio)
   }
